ceec6985f6ab0bee1b0f0e9f93d48a0dda3fa2e7
michalsalamucha <128612145+michalsalamucha@users.noreply.github.com>
2023-03-23 08:30:21 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,13 +1,24 @@
 Redmine::Plugin.register :redmine_home_page_redirector do
   name 'Home Page Redirector'
-  author 'Jean-Marc Lagacé'
-  description 'When a user is signed-in. The Home link sends to My Page.'
-  version '1.0.0'
+  author 'Jean-Marc Lagac'
+  description 'When a user is signed-in. The Home link sends to My Page. Gdy użytkownik jest zalogowany. Link strona główna kieruje do strony projekty . '
+  version '2.0.0'
   url 'https://github.com/jmlagace/redmine_home_page_redirector'
   author_url 'http://m2i3.com'
 
   require_dependency 'home_page_redirector'
-  
+
+
+if Rails.version > '6.0'
+  if Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk
+    Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
+  end
+  require File.dirname(__FILE__) + '/lib/home_page_redirector'
+
+  WelcomeController.send(:include, HomePageRedirector::HomePageRedirector)
+else
+  require_dependency 'home_page_redirector'
+
   # Here I have support for Redmine 1.x by falling back on Rails 2.x implementation.
   if Gem::Version.new("3.0") > Gem::Version.new(Rails.version) then
     Dispatcher.to_prepare do
@@ -21,4 +32,5 @@ Redmine::Plugin.register :redmine_home_page_redirector do
       WelcomeController.send(:include, HomePageRedirector::HomePageRedirector)
     end
   end
+end
 end
